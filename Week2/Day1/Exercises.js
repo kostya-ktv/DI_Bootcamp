4c2a7048b1e5e741ea3da812c262d3cb0d731526
Kostya Kotov <kotov25@yandex.ru>
2021-12-13 11:58:12 +0200

    ExercisesXP

--- Week2/Day1/Exercises.js
+++ Week2/Day1/Exercises.js
@@ -107,6 +107,6 @@
     // fruits.reverse()
     // console.log(fruits);
 
-   // #################################Exercise 2:
-    let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-    console.log(moreFruits[1][1])
+//    // #################################Exercise 2:
+//     let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+//     console.log(moreFruits[1][1])
