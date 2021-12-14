3f68670493d0d225d906334d0355f20205cd3222
Kostya Kotov <kotov25@yandex.ru>
2021-12-14 19:05:21 +0200

    bubble

---
+++ Week2/Day3/DailyChallengeStars/app.js
@@ -0,0 +1,24 @@
+// Instructions
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out “nested for loops” on Google).
+// Do this Daily Challenge by youself, without looking at the answers on the internet.
+
+
+// let count = 0;
+// let res = "";
+// while(count < 6){
+//     res += " *"
+//     console.log(res);
+//     count++;
+// }
+
+
+// let length = 6;
+// let res = "";
+// for(let i = 0; i < length; i++){
+//     for(let x = 0; x <= i; x++){
+//         res += " *";
+//     }
+//     res += "\n";
+// }
+// console.log(res);
