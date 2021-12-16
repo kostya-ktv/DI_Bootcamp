4ae589f4e3d8bb55795595c6cc6f6525e77b14b7
Kostya Kotov <kotov25@yandex.ru>
2021-12-16 12:24:22 +0200

    w2d5DailyChallenge

---
+++ Week2/Day5/DailyChallenge-99BottlesOfBeer/app.js
@@ -0,0 +1,18 @@
+let number = prompt("enter a number of bottles")
+let bottles = number;
+for(let i = 1; i <= number; i++){
+    if(printSong(bottles, i) == false) break;
+}
+
+function printSong(countBottles, countDownBottles){
+    console.log(`${countBottles} bottles of beer on the wall`)
+    console.log(`${countBottles} bottles of beer`)
+
+    countDownBottles == 1 ? console.log(`Take ${countDownBottles} down, pass it around`) : console.log(`Take ${countDownBottles} down, pass them around`)
+    if(countBottles - countDownBottles <= 0) {
+        console.log("no more bottles");
+        return false;
+    }
+    console.log(`${countBottles - countDownBottles} bottles of beer on the wall \n`)
+     bottles = countBottles - countDownBottles;
+}
