ac94acc8a37b2cc6ae2e472ce4c36aa01a90209c
Kostya Kotov <kotov25@yandex.ru>
2021-12-20 21:16:23 +0200

    dd

--- Week3/Day2/DailyChallenge/app.js
+++ Week3/Day2/DailyChallenge/app.js
@@ -1,5 +1,3 @@
-// "When I go to school, I eat my (noun), then I brushed my (landmark). Finally I grabbed my (barbershop item) and took the (fruit)
-// "Every day, I feed my cat (name of person) some (name of a cereal). He likes it so much that he (past tense verb) all over the (around the house).
 let noun = document.querySelector('#noun')
 let adjective = document.querySelector('#adjective')
 let names = document.querySelector('#person')
@@ -8,26 +6,35 @@ let place = document.querySelector('#place')
 let button = document.querySelector('#lib-button')
 let shuffle = document.querySelector('#shuffle')
 let values = []
-button.addEventListener('click', (e)=>{
-    e.preventDefault();
-    
-    let p = document.createElement('p')
-    p.innerText = generatedStory(noun.value, adjective.value, names.value, verb.value, place.value);
-    document.querySelector('#story').lastElementChild != null ? document.querySelector('#story').lastElementChild.remove() : ""
-    document.querySelector('#story').append(p)
-    document.querySelectorAll('input').forEach(e => {
-        values.push(e.value)
-        e.value = ""})
-})
+let inputs = [];
 
-shuffle.addEventListener('click', ()=>{
+document.querySelectorAll('input').forEach(e => {inputs.push(e);})
+
+button.addEventListener('click', generate)  
+
+shuffle.addEventListener('click', shuffleStory)
+
+
+function shuffleStory(){
     let p = document.createElement('p');
     p.innerText = generatedStory(...values);
     document.querySelector('#story').lastElementChild != null ? document.querySelector('#story').lastElementChild.remove() : ""
     document.querySelector('#story').append(p)
-})
+}
 
-
+function generate(){
+    if(inputs.every(e => e.value != '')){
+        let p = document.createElement('p')
+        p.innerText = generatedStory(noun.value, adjective.value, names.value, verb.value, place.value);
+        document.querySelector('#story').lastElementChild != null ? document.querySelector('#story').lastElementChild.remove() : ""
+        document.querySelector('#story').append(p)
+        document.querySelectorAll('input').forEach(e => {
+            values.push(e.value)
+            e.value = ""})
+        }else {
+            alert('fill inputs')
+        }
+}
 function generatedStory(someNone, someAdjective, someName, someVerb, somePlace){
     let stories = [
         `Hi iam ${someName}.When I go to ${somePlace}, I eat my ${someNone}. Suddenly I saw a ${someAdjective} car and I decided to ${someVerb} it`,
