d7f4ecd5d7f307ccab0e947db5c00c86a833df49
Kostya Kotov <kotov25@yandex.ru>
2021-12-20 09:11:16 +0200

    daily

---
+++ Week3/Day1/DailyChallengePlanets/app.js
@@ -0,0 +1,62 @@
+let planets = [
+    {
+        name :'Mercury',
+        moon: ['1','2','3']
+    },
+    {
+        name :'Venus',
+        moon: ['1','2','3']
+    },
+    {
+        name :'Neptune',
+        moon: ['1','2','3']
+    },
+    {
+        name :'Uranus',
+        moon: ['1','2','3']
+    },
+    {
+        name :'Saturn',
+        moon: ['1','2','3']
+    },
+    {
+        name :'Jupiter',
+        moon: ['1','2','3']
+    },
+    {
+        name :'Mars',
+        moon: ['1','2','3']
+    },
+    {
+        name :'Earth',
+        moon: ['1','2','3']
+    }
+]
+
+
+for(let el of planets){
+    let planet = document.createElement('div');
+    planet.classList.add('planet');
+    planet.innerHTML = el.name;
+    planet.style.color = 'white'
+    planet.style.backgroundColor = getRandomColor();
+    document.querySelector('section').append(planet)
+
+    el.moon.forEach(m => {
+        let moon = document.createElement('div');
+        moon.classList.add('moon');
+        
+        planet.append(moon);
+    })
+    
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let  i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
