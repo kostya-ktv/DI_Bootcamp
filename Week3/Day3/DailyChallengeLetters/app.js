c14cf972b1cc2d2edc66c22ced3924849339d46f
Kostya Kotov <kotov25@yandex.ru>
2021-12-22 09:23:14 +0200

    dailyW3D3

--- Week3/Day3/DailyChallengeLetters/app.js
+++ Week3/Day3/DailyChallengeLetters/app.js
@@ -1,2 +1,1 @@
-// document.querySelector('input').onkeydown = (event) => {return /[a-z]/i.test(event.key)};
-document.querySelector('input').addEventListener('keydown', (e)=>{return /[a-z]/i.test(e.key)})
+document.querySelector('input').onkeydown = (event) => {return /[a-z]/i.test(event.key)};
