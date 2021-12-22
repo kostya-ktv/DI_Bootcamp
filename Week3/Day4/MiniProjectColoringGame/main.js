0c6ae0ccce53e87bcfc4986d0b98d023e01b3425
Kostya Kotov <kotov25@yandex.ru>
2021-12-22 11:33:55 +0200

    w3d4

---
+++ Week3/Day4/MiniProjectColoringGame/main.js
@@ -0,0 +1,202 @@
+let colorNames = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+  ];
+let mainDiv = document.createElement('div');
+let colorDiv = document.createElement('div');
+let gridDiv = document.createElement('div');
+let button = document.createElement('button');
+let colors = document.createElement('div');
+let arrayOfColors = ['black', 'green'];
+let currentColor = "";
+let mouseDown = false;
+
+button.innerText = "CLEAR";
+colors.classList.add('colorBox');
+mainDiv.classList.add('main');
+colorDiv.classList.add('colorsDiv');
+colorDiv.append(button, colors);
+gridDiv.classList.add('gridDiv');
+
+document.body.append(mainDiv);
+mainDiv.append(colorDiv, gridDiv);
+
+for(let i = 0; i < 900; i++){
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.addEventListener('mousedown', setColor)
+    cell.addEventListener('mouseover', overColor)
+    cell.addEventListener('mouseup', ()=> mouseDown = false)
+    
+    gridDiv.append(cell);
+}
+for(let i = 0; i < 21; i++){
+    let colorCell = document.createElement('div');
+    colorCell.classList.add('colorCell')
+    colorCell.style.backgroundColor = colorNames[i + 10];
+    colorCell.addEventListener('click', chooseColor)
+    colors.append(colorCell);
+}
+button.addEventListener('click', ()=>{
+    document.querySelectorAll('.cell').forEach(e=> e.style.backgroundColor = "white")
+})
+
+function chooseColor(e){
+    currentColor = e.target.style.backgroundColor
+}
+function setColor(e){
+    mouseDown = true;
+    e.target.style.backgroundColor = currentColor;
+}
+function overColor(e){
+
+    if(mouseDown) e.target.style.backgroundColor = currentColor;
+}
+
+
