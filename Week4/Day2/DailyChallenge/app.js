dba2bf3aa2d10e4e79ec093059e79ab1fac3d682
Kostya Kotov <kotov25@yandex.ru>
2021-12-28 20:58:06 +0200

    w4w3

--- Week4/Day2/DailyChallenge/app.js
+++ Week4/Day2/DailyChallenge/app.js
@@ -1,14 +1,14 @@
-let groceries = {
-    fruits : ["pear", "apple", "banana"],
-    vegetables: ["tomatoes", "cucumber", "salad"],
-    totalPrice : "20$",
-    other : {
-        payed : true,
-        meansOfPayment : ["cash", "creditCard"]
-    }
-}
-let groceriesCopy = groceries;
-groceries.totalPrice = "30$";
-groceries.other.payed = false;
-console.log(groceriesCopy.totalPrice);
-console.log(groceriesCopy.other.payed);
+// let groceries = {
+//     fruits : ["pear", "apple", "banana"],
+//     vegetables: ["tomatoes", "cucumber", "salad"],
+//     totalPrice : "20$",
+//     other : {
+//         payed : true,
+//         meansOfPayment : ["cash", "creditCard"]
+//     }
+// }
+// let groceriesCopy = groceries;
+// groceries.totalPrice = "30$";
+// groceries.other.payed = false;
+// console.log(groceriesCopy.totalPrice);
+// console.log(groceriesCopy.other.payed);
