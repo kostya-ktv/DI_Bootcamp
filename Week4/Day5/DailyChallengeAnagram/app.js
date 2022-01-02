d8c7f72948a899d198a86c08c7698ede812bbaff
Kostya Kotov <kotov25@yandex.ru>
2022-01-02 10:15:07 +0200

    dd

--- Week4/Day5/DailyChallengeAnagram/app.js
+++ Week4/Day5/DailyChallengeAnagram/app.js
@@ -1,11 +1,14 @@
 function checkingAnagram(string1, string2) {
-    if(string1.length !== string2.length) return false;
-    let victim = string2.trim().toLowerCase().split('');
+//sorry. not very well understood what anagram is :D :D :D 
+// but for anagram it works too 
 
-        return string1.trim().toLowerCase().split('').every(el => {
+    if(string1.length !== string2.length) return false;
+    let victim = string2.trim().toLowerCase().split('');       
+    return string1.trim().toLowerCase().split('').every(el => {
             if(victim.lastIndexOf(el) === -1) return false;
             return victim.splice(victim.lastIndexOf(el), 1);
         });
 }
 
-console.log(checkingAnagram('School mastjr', 'The classroom'));
+
+console.log(checkingAnagram('School master', 'The classroom'));
