ff7d716816c2c8f26671b28a4feb7eb468d97900
Kostya Kotov <kotov25@yandex.ru>
2022-01-03 21:55:42 +0200

    dda

---
+++ Week5/Day1/ExercisesXP/main.js
@@ -0,0 +1,40 @@
+// Exercise 1 : HTML Form
+// Where will the sent data appear?
+// ------UrlSearchParams
+
+// Exercise 2 : HTML Form #2
+// Instructions
+// Use the same form structure as Exercise 1.
+// When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
+// Where will the sent data appear? Hint : Look at the Network Tab
+//------NETWORK-INDEX.HTML-PAYLOAD
+
+// Exercise 3 : JSON Mario
+// let marioGame = {
+//     detail: "An amazing game!",
+//     characters: {
+//         mario: {
+//             description: "Small and jumpy. Likes princesses.",
+//             height: 10,
+//             weight: 3,
+//             speed: 12,
+//         },
+//         bowser: {
+//             description: "Big and green, Hates princesses.",
+//             height: 16,
+//             weight: 6,
+//             speed: 4,
+//         },
+//         princessPeach: {
+//             description: "Beautiful princess.",
+//             height: 12,
+//             weight: 2,
+//             speed: 2,
+//         }
+//     },
+// }
+
+
+// let toObject = JSON.stringify(marioGame);
+// console.log(toObject);
+
