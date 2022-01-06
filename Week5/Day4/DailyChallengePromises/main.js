21f0bf826763351d55fe6632d6360ea5cf2ee018
Kostya Kotov <kotov25@yandex.ru>
2022-01-06 08:03:29 +0200

    dailyw5d4

---
+++ Week5/Day4/DailyChallengePromises/main.js
@@ -0,0 +1,7 @@
+let urls = [
+    'https://jsonplaceholder.typicode.com/todos/1',
+    'https://jsonplaceholder.typicode.com/todos/2',
+    'https://jsonplaceholder.typicode.com/todos/3',
+    'https://jsonplaceholder.typicode.com/todos/4',
+];
+Promise.all(urls.map(el => fetch(el).then(data => data.json()))).then(result => console.log(result)).catch(error => console.log(error));
