fdf6c09b14af53c153cc968fa20525dd401ce8a3
Kostya Kotov <kotov25@yandex.ru>
2022-01-06 09:48:58 +0200

    fix

--- Week5/Day4/DailyChallengePromises/main.js
+++ Week5/Day4/DailyChallengePromises/main.js
@@ -4,4 +4,8 @@ let urls = [
     'https://jsonplaceholder.typicode.com/todos/3',
     'https://jsonplaceholder.typicode.com/todos/4',
 ];
-Promise.all(urls.map(el => fetch(el).then(data => data.json()))).then(result => console.log(result)).catch(error => console.log(error));
+Promise.all(urls.map(el => fetch(el)
+        .then(data => data.json())
+    ))
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
