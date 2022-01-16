d686ccd33e2171671301e9eef9510c359d5bab2b
Kotov Konstantin <kotov25@yandex.ru>
2022-01-16 12:30:50 +0200

    w7D1

---
+++ Week7/Day1/ExercisesXP/npm-node/main.js
@@ -0,0 +1,11 @@
+// let a = 5;
+// let b = 10;
+// console.log("Hi you are " + (a + b));
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+console.log("First Run : " + (x));
+console.log("Second Run : " + (x + c));
